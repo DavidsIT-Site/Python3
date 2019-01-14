337e086278b611a00d2cdae97a76f5a83570b41a
David <David@DavidsIT.Site>
2019-01-14 17:31:43 -0500

    more learning/testing

--- Common_Questions/ch1_5.py
+++ Common_Questions/ch1_5.py
@@ -35,3 +35,56 @@ print(q.pop())
 print(q.pop())
 print(q.pop())
 print(q.pop())
+
+#1.6 mapping keys
+d ={
+    'a': [1,2,3],
+    'b': [6,4]
+}
+
+e = {
+    'a': {1, 2, 3},
+    'b': {4,6}
+}
+print(d)
+
+from collections import defaultdict
+d = defaultdict(list)
+d['a'].append(1)
+d['a'].append(2)
+d['b'].append(5)
+print(d)
+d = defaultdict(set)
+d['a'].add(2)
+print(d)
+
+d = {}
+d.setdefault('a', []).append(1)
+d.setdefault('a', []).append(2)
+d.setdefault('b', []).append(2)
+
+print(d)
+
+#discussion about multivalued dictionary
+pairs = ([('foo', 1), ('bar', 2)])
+d = {}
+for key, value, in pairs:
+    if key not in d:
+        d[key] = []
+    d[key].append(value)
+
+#pythonic
+d = defaultdict(list)
+for key, value in pairs:
+    d[key].append(value)
+
+from collections import OrderedDict
+d = OrderedDict()
+d['foo'] = 1
+d['bar'] = 2
+for key in d:
+    print(key, d[key])
+import json
+print(json.dumps(d))
+
+

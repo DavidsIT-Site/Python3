43dc89a532a91a98200f2da59f23ad1f084119d3
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-01-14 21:27:08 -0500

    groupby

---
+++ Common_Questions/ch1_15.py
@@ -0,0 +1,17 @@
+rows = [
+    {'address': '5412 N Clark', 'date': '07/01/2012'},
+    {'address': '5148 N Clark', 'date': '07/04/2012'},
+    {'address': '5890 E 57th', 'date': '07/02/2012'},
+    {'address': '5415 N Clark', 'date': '07/01/2012'},
+    {'address': '5143 N Clark', 'date': '07/04/2012'},
+    {'address': '5891 E 57th', 'date': '07/02/2012'}
+
+]
+from operator import itemgetter
+from itertools import groupby
+
+rows.sort(key=itemgetter('date'))
+for date, items in groupby(rows, key=itemgetter('date')):
+    print(date)
+    for item in items:
+        print('     ', item)

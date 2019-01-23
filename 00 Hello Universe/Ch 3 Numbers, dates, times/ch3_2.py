c7de29f50f4429866d49cddc56186d229e0d2c9e
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-01-23 10:52:29 -0500

    # formatting numbers for output

---
+++ 00 Hello Universe/Ch 3 Numbers, dates, times/ch3_2.py
@@ -0,0 +1,26 @@
+# decimal calculations
+a = 3.1
+b = 4.2
+print(a+b)
+
+from decimal import Decimal
+a = Decimal('3.1')
+b = Decimal('4.2')
+print(a+b)
+
+from decimal import localcontext
+a = 3.1
+b = 4.2
+print(a/b)
+a = Decimal('3.1')
+b = Decimal('4.2')
+print(a/b)
+
+with localcontext() as ctx:
+    ctx.prec = 3
+    print(a/b)
+
+
+with localcontext() as ctx:
+    ctx.prec = 53
+    print(a/b)
---
+++ 00 Hello Universe/Ch 3 Numbers, dates, times/ch3_3.py
@@ -0,0 +1,8 @@
+# formatting numbers for output
+
+x = 2134.58973
+print(format(x, '0.2f'))
+print(format(x, '>10.1f'))
+print(format(x, '<10.1f'))
+print(format(x, '^10.1f'))
+print(format(x, '^50,.1f'))

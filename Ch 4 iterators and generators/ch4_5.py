3b5f8e59910494d83678f1f9752f953dc7be29c5
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-01-28 11:27:59 -0500

    # iterating in reverse

---
+++ Ch 4 iterators and generators/ch4_5.py
@@ -0,0 +1,27 @@
+# iterating in reverse
+
+a = [1, 2, 3, 4, 5]
+for x in reversed(a):
+    print(x)
+
+f = open('passwd')
+for line in reversed(list(f)):
+    print(line)
+
+
+
+class Countdown:
+    def __init__(self, start):
+        self.start = start
+
+    def __iter__(self):
+        n = self.start
+        while n > 0:
+            yield n
+            n -= 1
+
+    def __reversed__(self):
+        n = 1
+        while n <= self.start:
+            yield n
+            n += 1

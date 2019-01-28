34fb21fa4a40c637846fe54858314d26f8ec080f
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-01-28 15:54:27 -0500

    # printing with a different separator or line ending

--- Ch 5 Files and IO/ch5_3.py
+++ Ch 5 Files and IO/ch5_3.py
@@ -1,1 +1,7 @@
 # printing with a different separator or line ending
+print('ACME', 50, 91.5)
+print('ACME', 50, 91.5, sep=', ')
+print('ACME', 50, 91.5, sep=', ', end="!!\n")
+
+row = ('ACME', 50, 91.5)
+print(*row, sep=',')

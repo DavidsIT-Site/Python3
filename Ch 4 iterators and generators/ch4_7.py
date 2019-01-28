ae64b13d752447cc86d026d59004ce3fd00f903d
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-01-28 13:06:29 -0500

    # islice consumes data. can't undo iterators. maybe a list instead

--- Ch 4 iterators and generators/ch4_7.py
+++ Ch 4 iterators and generators/ch4_7.py
@@ -1,1 +1,13 @@
 # you want sliced data from an iterator but normal slicing [y:z] doesn't work
+def count(n):
+    while True:
+        yield n
+        n += 1
+
+c = count(0)
+#c[10:20]
+import itertools
+for x in itertools.islice(c,10,20):
+    print(x)
+
+    # islice consumes data. can't undo iterators. maybe a list instead

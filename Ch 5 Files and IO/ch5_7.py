82cb38f37f0e9e6b22de74e51453c42a1e42ac5e
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-01-28 16:30:04 -0500

    # reading and writing compressed datafiles # gzip or bz2 compression

--- Ch 5 Files and IO/ch5_7.py
+++ Ch 5 Files and IO/ch5_7.py
@@ -1,2 +1,28 @@
 # reading and writing compressed datafiles
 # gzip or bz2 compression
+
+# write compressed data to file
+text = "my block text is not blocky enough"
+
+import gzip
+with gzip.open('somefile.gz', 'wt') as f:
+    f.write(text)
+
+text = "my block text is not blocky enough2"
+import bz2
+
+with bz2.open('somefile.bz2', 'wt') as f:
+    f.write(text)
+
+with gzip.open('somefile.gz', 'rt') as f:
+    print(f.read())
+
+with bz2.open('somefile.bz2', 'rt') as f:
+    print(f.read())
+
+
+#compression modules work with various file like objects: sockets, pipes and in memory files
+
+f = open('somefile.gz', 'rb')
+with gzip.open(f, 'rt') as g:
+    text = g.read()

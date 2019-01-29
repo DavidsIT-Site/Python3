84d2e689762d07598e65e3ac2c46f9c8f1a4c240
David <David@DavidsIT.Site>
2019-01-29 14:32:42 -0500

    # Reading and writing JSON data import json

--- Ch 6 Data Encoding and processing/ch6_2.py
+++ Ch 6 Data Encoding and processing/ch6_2.py
@@ -1,2 +1,24 @@
 # Reading and writing JSON data
 import json
+
+data = {
+    'Symbol': 'AA',
+    'Price': '39.48',
+    'Change': '-.18'
+}
+json_str = json.dumps(data)
+print(data)
+print(json_str)
+data_j2d = json.loads(json_str)
+
+with open('ch6_2.json', 'w') as f:
+    json.dump(data, f)
+
+with open('ch6_2.json', 'r') as f:
+    data_readin = json.load(f)
+
+print(data_readin)
+
+
+#doesn't work
+#json link bad

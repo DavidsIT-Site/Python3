48f5712c6bf86f7875c40d884dc6065e156af672
David <David@DavidsIT.Site>
2019-02-08 17:23:04 -0500

    # creating a new kind of class or instance attribute

--- Ch 8 Classes and Objects/ch8_9.py
+++ Ch 8 Classes and Objects/ch8_9.py
@@ -1,4 +1,14 @@
 # creating a new kind of class or instance attribute
 
 class Integer:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
 

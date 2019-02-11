93966ce2efb2e69d3d2167a4f3f6b34cfc662ea1
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-02-11 14:38:25 -0500

    Mixin classes appear in various places in the standard library, mostly as a means for extending the functionality of other classes similar to as shown. They are also one of the main uses of multiple inheritance. For instance, if you are writing network code, you can often use the ThreadingMixIn from the socketserver module to add thread support to other network-related classes

--- Ch 8 Classes and Objects/ch8_18.py
+++ Ch 8 Classes and Objects/ch8_18.py
@@ -1,5 +1,9 @@
 #extending classes with mixins
 
+
+# These classes, by themselves, are useless. In fact, if you instantiate any one of them, it
+# does nothing useful at all (other than generate exceptions). Instead, they are supposed
+# to be mixed with other mapping classes through multiple inheritance.
 # Mixin classes appear in various places in the standard library, mostly as a means for
 # extending the functionality of other classes similar to as shown. They are also one of
 # the main uses of multiple inheritance. For instance, if you are writing network code,

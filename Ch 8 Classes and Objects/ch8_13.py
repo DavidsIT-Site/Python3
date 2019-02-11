4536d11177959d8a69baa77c8d7bb37a5edbb2c5
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-02-11 13:52:38 -0500

    # implementing a data model or type system # Covering the basics of all # those topics is beyond what can be covered here, but examples can be found in other # recipes (see Recipes 8.9, 8.18, 9.12, and 9.19). However, there are a number of subtle # points worth noting.

---
+++ Ch 8 Classes and Objects/ch8_13.py
@@ -0,0 +1,5 @@
+# implementing a data model or type system
+# Covering the basics of all
+# those topics is beyond what can be covered here, but examples can be found in other
+# recipes (see Recipes 8.9, 8.18, 9.12, and 9.19). However, there are a number of subtle
+# points worth noting.

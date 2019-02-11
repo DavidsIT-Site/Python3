9818cf8e870652697a1d9cbfb8f7ca2dddd3152e
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-02-11 14:29:51 -0500

    When creating instances in a nonstandard way, it’s usually best to not make too many assumptions about their implementation. As such, you generally don’t want to write code that directly manipulates the underlying instance dictionary __dict__ unless you know it’s guaranteed to be defined. Otherwise, the code will break if the class uses __slots__, properties, descriptors, or other advanced techniques. By using se tattr() to set the values, your code will be as general purpose as possible.

---
+++ Ch 8 Classes and Objects/ch8_17.py
@@ -0,0 +1,15 @@
+# bypassing init
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+d = Date.__new__(Date)
+print(d)
+# print(d.year)
+data = {'year': 2012, 'month': 8, 'day': 29}
+for key, value in data.items():
+    setattr(d, key, value)
+
+print(d.year)

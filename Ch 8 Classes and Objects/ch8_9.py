73c5a045b7b9e256702cdff31c1bc8a4b422b6ea
DavidsIT-Site <44936577+DavidsIT-Site@users.noreply.github.com>
2019-02-11 11:16:33 -0500

    https://python-forum.io/Thread-Unexpected-expected-type-error-result

--- Ch 8 Classes and Objects/ch8_9.py
+++ Ch 8 Classes and Objects/ch8_9.py
@@ -1,4 +1,4 @@
-# creating a new kind of class or instance attribute
+# creating a new kind of class or instance attribute# creating a new kind of class or instance attribute
 
 class Integer:
     def __init__(self, name):
@@ -11,4 +11,31 @@ class Integer:
             return instance.__dict__[self.name]
 
     def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError('expected an int')
+        instance.__dict__[self.name] = value
 
+
+    def __delete__(self, instance):
+        del instance.__dict__[self]
+
+
+class Point:
+    x = Integer('x')
+    y = Integer('y')
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+p = Point(12, 3)
+my_int = Integer(1)
+print('myint:   {}   {}'.format(my_int, my_int.name))
+print('{}   {}'.format(p.x, p.y))
+
+
+my_int2 = Integer('1')
+print('myint2:  {}   {}'.format(my_int2, my_int2.name))
+# p2 = Point('1', 3)
+
+# https://python-forum.io/Thread-Unexpected-expected-type-error-result

8044af40d0b9d32971f1471cc70f57a227da0c31
davidsit-site <Github96!>
2019-02-18 14:40:11 -0500

    All things being equal, include the __init__.py files if you’re just starting out with the creation of a new package.

---
+++ Ch 10 Modules and Packages/ch10_1.py
@@ -0,0 +1,2 @@
+# All things being equal, include the
+# __init__.py files if you’re just starting out with the creation of a new package.
--- Ch 9 Metaprogramming/ch9_24.py
+++ Ch 9 Metaprogramming/ch9_24.py
@@ -1,2 +1,10 @@
 # parsing and analyzing python source
 
+x = 42
+var1 = eval('2 + 2*3 + x')
+print(var1)
+
+import ast
+ex = ast.parse('2 + 2*3 + x', mode='eval')
+print(ex)
+print(ast.dump((ex)))

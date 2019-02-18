bf8db830b5cd8f91ff6722920227321412d1190c
davidsit-site <Github96!>
2019-02-18 14:52:34 -0500

    loading remote modules using import hooks

---
+++ Ch 10 Modules and Packages/ch10_11.py
@@ -0,0 +1,3 @@
+# loading modules from a remote machine using import hooks
+# First, a serious disclaimer about security. The idea discussed in this recipe would be wholly bad
+# without some kind of extra security and authentication layer

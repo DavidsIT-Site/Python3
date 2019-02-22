3e46d1c89c0c475ca31cdbfc05ced868dc4b2652
davidsit-site <Github96!>
2019-02-22 11:36:28 -0500

    issuing warning messages

---
+++ Ch 14 Testing/ch14_11.py
@@ -0,0 +1,3 @@
+The arguments to warn() are a warning message along with a warning class, which is typically one of
+the following: UserWarning, DeprecationWarning, SyntaxWarning, RuntimeWarning,
+ResourceWarning, or FutureWarning
